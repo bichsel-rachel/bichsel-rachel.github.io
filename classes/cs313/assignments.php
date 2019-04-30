a097bd7178d2a435c6ccd49220071da5479e472e
Rachel Bichsel <rachbichsel@gmail.com>
2019-04-30 00:38:07 -0600

    kk

---
+++ classes/cs313/assignments.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles/main.css">
+        <link rel="stylesheet" href="styles/normalize.css">
+        <title>Assignments</title>
+    </head>
+    <body>
+        <header>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="about.php">About</a></li>
+                 <li><a href="assignments.php">Assignments</a></li>
+            </ul>
+        </nav>
+        <main>
+            <h1>Coming Soon!</h1>
+        </main>
+    </body>
+</html>
