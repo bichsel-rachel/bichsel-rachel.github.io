af6b3a5887fcdabee29593473a9db3487ffadabd
Rachel Bichsel <rachbichsel@gmail.com>
2019-05-16 12:26:50 -0600

    css

---
+++ classes/cit261/css-javascript/style.js
@@ -0,0 +1,15 @@
+function force(){ document.getElementById("force").style.color="red";
+document.getElementById("force").style.fontWeight="bold";
+document.getElementById("force").style.fontSize="2em";
+}
+
+function toParallel(){
+    document.getElementById("square").style.transform="skew(20deg)";
+    document.getElementById("square").style.transition="all 2s";
+}
+
+function toSquare(){
+    document.getElementById("square").style.transform="none";
+}
+
+
