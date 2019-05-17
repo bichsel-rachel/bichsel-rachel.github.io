2e0f887e64e5b1cf5fcb038ce91a14aa32299f44
Rachel Bichsel <rachbichsel@gmail.com>
2019-05-17 16:15:55 -0600

    css

--- classes/cit261/css-javascript/style.js
+++ classes/cit261/css-javascript/style.js
@@ -1,15 +1,22 @@
-function force(){ document.getElementById("force").style.color="red";
-document.getElementById("force").style.fontWeight="bold";
-document.getElementById("force").style.fontSize="2em";
+function force() {
+    document.getElementById("force").style.color = "red";
+    document.getElementById("force").style.fontWeight = "bold";
+    document.getElementById("force").style.fontSize = "2em";
 }
 
-function toParallel(){
-    document.getElementById("square").style.transform="skew(20deg)";
-    document.getElementById("square").style.transition="all 2s";
+function toParallel() {
+    document.getElementById("square").style.transform = "skew(20deg)";
+    document.getElementById("square").style.transition = "all 2s";
 }
 
-function toSquare(){
-    document.getElementById("square").style.transform="none";
+function toSquare() {
+    document.getElementById("square").style.transform = "none";
 }
 
+function elephant() {
+    var size = document.getElementById("size").value;
+     document.getElementById("change").style.fontSize = size;
+}
+ 
 
+   
