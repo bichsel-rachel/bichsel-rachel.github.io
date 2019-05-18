1b7c35fce3362f0de633b239e8e2323d1b8a1cd0
Rachel Bichsel <rachbichsel@gmail.com>
2019-05-17 21:40:59 -0600

    comments

--- classes/cit261/css-javascript/style.js
+++ classes/cit261/css-javascript/style.js
@@ -1,9 +1,11 @@
+//Example 1 code
 function force() {
     document.getElementById("force").style.color = "red";
     document.getElementById("force").style.fontWeight = "bold";
     document.getElementById("force").style.fontSize = "2em";
 }
 
+//Example 2 code
 function toParallel() {
     document.getElementById("square").style.transform = "skew(20deg)";
     document.getElementById("square").style.transition = "all 2s";
@@ -13,6 +15,8 @@ function toSquare() {
     document.getElementById("square").style.transform = "none";
 }
 
+
+//Example 3 code
 function elephant() {
     var size = document.getElementById("size").value;
      document.getElementById("change").style.fontSize = size;
