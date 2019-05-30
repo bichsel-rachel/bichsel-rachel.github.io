0a597ab17998d8177251d7b23b5090166bcb71a8
Rachel Bichsel <rachbichsel@gmail.com>
2019-05-30 15:17:29 -0600

    updaet

--- classes/cit261/ajax/script.js
+++ classes/cit261/ajax/script.js
@@ -5,7 +5,7 @@ btn.addEventListener("click", function() {
 //XMLHttpRequest is a built in browser tool. Establish connection with URL and lets send and store data.
 var ourRequest = new XMLHttpRequest();
 //tell browser to go to URL and get JSON data. Use GET to get data. second parameter is URL you want to get.
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.open('GET', 'https://bichsel-rachel.github.io/classes/cit261/ajax/lor.json');
 //
 ourRequest.onload = function() {
     //console.log(ourRequest.responseText);
@@ -21,7 +21,7 @@ function renderHTML(data) {
     var htmlString = "";
     
     for (i = 0; i < data.length; i++){
-        htmlString += "<p>" + data[i].name + " is a " + data[i].race + ".</p>";
+        htmlString += "<p>" + data[i].name + " is a " + data[i].race + ". He likes " + data[i].facts.likes + ". He dislikes " + data[i].facts.dislikes + ".</p>";
     }
   animalContainer.insertAdjacentHTML('beforeend', htmlString);  
 }
